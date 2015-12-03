0159fc17b7f3b73a3ece2f37f00e5a84c932970b
bulbul.hossen <info247.bulbul@gmail.com>
2015-12-04 01:22:46 +0600

    Modified MainActivity.Class and add Splash.Class
    TextView
    
    Button Add

--- app/src/main/java/com/example/bulbulhossen/edittext/MainActivity.java
+++ app/src/main/java/com/example/bulbulhossen/edittext/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.bulbulhossen.edittext;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editText;
-
+    Button button;
     TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
